d12551fad3727e90a875acade96b111ae3305dbe
yyhyan100 <yyhyan@gmail.com>
2020-12-15 19:45:41 -0600

    pressure outlet condition added

--- bc.f90
+++ bc.f90
@@ -2,6 +2,7 @@ subroutine bc()
 !call non_eq_extra()
 call inlet()
 call bouncebk()
+call outlet_pressure()
 end subroutine
 
 subroutine inlet() ! Zou-He boundary condition for velocity inlet
@@ -12,6 +13,34 @@ subroutine inlet() ! Zou-He boundary condition for velocity inlet
 		f(1,i,j)=f(3,i,j)+2*rho(i,j)*u(i,j)/3.0
 		f(5,i,j)=f(7,i,j)-0.5*(f(2,i,j)-f(4,i,j))+rho(i,j)*u(i,j)/6.0
 		f(8,i,j)=f(6,i,j)+0.5*(f(2,i,j)-f(4,i,j))+rho(i,j)*u(i,j)/6.0
+	enddo
+end subroutine
+!----------------------------------------------------------------------------
+subroutine outlet_open() ! open boundary condition
+	use vars
+	i=ied
+	do j=1,jed
+!		do k=0,Q
+!			f(k,i,j)=2*f(k,i-1,j)-f(k,i-2,j)
+!		enddo
+		f(3,i,j)=2*f(3,i-1,j)-f(3,i-2,j)
+		f(6,i,j)=2*f(6,i-1,j)-f(6,i-2,j)
+		f(7,i,j)=2*f(7,i-1,j)-f(7,i-2,j)
+!		v(i,j)=0.0
+!		u(i,j)=2*u(i-1,j)-u(i-2,j)
+	enddo
+end subroutine
+!----------------------------------------------------------------------------
+subroutine outlet_pressure() ! pressure boundary condition
+	use vars
+	real rho_out
+	rho_out=0.8
+	i=ied
+	do j=1,jed
+		u(i,j)=(f(0,i,j)+f(2,i,j)+f(4,i,j)+2.0*(f(1,i,j)+f(5,i,j)+f(8,i,j)))/rho_out-1.0
+		f(3,i,j)=f(1,i,j)-2*rho_out*u(i,j)/3
+		f(6,i,j)=f(8,i,j)-0.5*(f(2,i,j)-f(4,i,j))-rho_out*u(i,j)/6.0
+		f(7,i,j)=f(5,i,j)+0.5*(f(2,i,j)-f(4,i,j))-rho_out*u(i,j)/6.0
 	enddo
 end subroutine
 !----------------------------------------------------------------------------
@@ -40,17 +69,7 @@ subroutine bouncebk
 !		f(8,i,j)=f(6,i,j)
 !	enddo
 ! right
-	i=ied
-	do j=1,jed
-!		do k=0,Q
-!			f(k,i,j)=2*f(k,i-1,j)-f(k,i-2,j)
-!		enddo
-		f(3,i,j)=2*f(3,i-1,j)-f(3,i-2,j)
-		f(6,i,j)=2*f(6,i-1,j)-f(6,i-2,j)
-		f(7,i,j)=2*f(7,i-1,j)-f(7,i-2,j)
-!		v(i,j)=0.0
-!		u(i,j)=2*u(i-1,j)-u(i-2,j)
-	enddo
+
 	
 ! corner points, bounce back -- test
 !	i=1;j=1
